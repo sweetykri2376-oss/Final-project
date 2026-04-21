0abe0c2c79a8d2666eea2ccc1298f3e8101c1029
Kumari <2478916@cognizant.com>
2026-04-21 16:08:31 +0530

    final update

--- pages/BugPage.ts
+++ pages/BugPage.ts
@@ -1,59 +1,42 @@
-import { Page, expect } from '@playwright/test';
+import { Page, expect } from "@playwright/test";
 
- 
 export class BugPage {
-    constructor(private page: Page) { }
- 
-    async submitBug(summary: string, description: string, resolution: string = '1') {
-        await this.page.locator('input[name="summary"]').fill(summary);
-        await this.page.locator('textarea[name="description"]').fill(description);
-        await this.page.selectOption('select[name="severity"]', '1');
-        await this.page.selectOption('select[name="priority"]', '1');
-        await this.page.selectOption('select[name="assigned_to"]', '6');
-        await this.page.selectOption('select[name="resolution"]', resolution);
-        await this.page.getByRole('button', { name: 'Submit' }).click();
+  constructor(private page: Page) {}
+
+  async submitBug(
+    summary: string,
+    description: string,
+    resolution: string = "1",
+  ) {
+    await this.page.locator('input[name="summary"]').fill(summary);
+    await this.page.locator('textarea[name="description"]').fill(description);
+    await this.page.selectOption('select[name="severity"]', "1");
+    await this.page.selectOption('select[name="priority"]', "1");
+    await this.page.selectOption('select[name="assigned_to"]', "6");
+    await this.page.selectOption('select[name="resolution"]', resolution);
+    await this.page.getByRole("button", { name: "Submit" }).click();
+  }
+
+  async populateResolutionOptions() {
+    const select = this.page.locator('select[name="resolution"]');
+    const count = await select.locator("option").count();
+    // const options = [];
+    for (let i = 0; i < count; i++) {
+      const option = select.locator("option").nth(i);
+      const value = await option.getAttribute("value");
+      const text = await option.textContent();
+
+      console.log(`Value: ${value}, Text: ${text?.trim()}`);
     }
- 
-    // async getResolutionOptions() {
-    //     const select = this.page.locator('select[name="resolution"]');
-    //     const count = await select.locator('option').count();
-    //     const options = [];
-    //     for (let i = 0; i < count; i++) {
-    //         const option = select.locator('option').nth(i);
-    //         const value = await option.getAttribute('value');
-    //         const text = await option.textContent();
-    //         options.push({ value, text: text?.trim() });
-    //     }
-    //     return options;
-    // }
- 
-    async populateResolutionOptions() {
-        // const options = await this.getResolutionOptions();
-        // // in stringify, the 3rd parameter is for indentation with 2 spaces
-        // const fileContent = `export const resolutionOptions = ${JSON.stringify(options, null, 2)};`;
-        // const filePath = path.join(__dirname, '..', 'dropDownListValues.ts');
-        // fs.writeFileSync(filePath, fileContent, 'utf8');
- 
-        const select = this.page.locator('select[name="resolution"]');
-        const count = await select.locator('option').count();
-        // const options = [];
-        for (let i = 0; i < count; i++) {
-            const option = select.locator('option').nth(i);
-            const value = await option.getAttribute('value');
-            const text = await option.textContent();
-            // options.push({ value, text: text?.trim() });
-            console.log(`Value: ${value}, Text: ${text?.trim()}`);
-        }
-    }
- 
-    // async updateBug() {
-    //   const viewBugLink = this.page.getByRole('link', { name: /View Bug/i }).first();
-    //   await expect(viewBugLink).toBeVisible();
-    //   await viewBugLink.click();
-    //   await this.page.selectOption('select[name="assigned_to"]', '3');
-    //   await this.page.selectOption('select[name="severity"]', '3');
-    //   await this.page.selectOption('select[name="priority"]', '2');
-    //   await this.page.getByRole('button', { name: 'Update bug' }).click();
-    // }
+  }
+
+  // async updateBug() {
+  //   const viewBugLink = this.page.getByRole('link', { name: /View Bug/i }).first();
+  //   await expect(viewBugLink).toBeVisible();
+  //   await viewBugLink.click();
+  //   await this.page.selectOption('select[name="assigned_to"]', '3');
+  //   await this.page.selectOption('select[name="severity"]', '3');
+  //   await this.page.selectOption('select[name="priority"]', '2');
+  //   await this.page.getByRole('button', { name: 'Update bug' }).click();
+  // }
 }
- 
--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -10,156 +10,134 @@ import { CreditCardPage } from "../pages/CreditCardPage";
 import Data from "../test.json";
 
 test.describe("Sharelane Tests", () => {
+  // This before each is going to open the sharelane website
+  // and also click the ENTER link.
+  test.beforeEach(async ({ page }) => {
+    // creating object with HomePage class to access its methods
+    const home = new HomePage(page);
+    await home.open();
+    await home.clickEnter();
+  });
 
-    // This before each is going to open the sharelane website
-    // and also click the ENTER link.
-    test.beforeEach(async ({ page }) => {
-        // creating object with HomePage class to access its methods
-        const home = new HomePage(page);
-        await home.open();
-        await home.clickEnter();
+  // Below test is for successful creation of account
+  test("Happy Path Account Creation @smoke @ui", async ({ page }, testInfo) => {
+    // home and auth are objects created using the classes mentioned below
+    // const home = new HomePage(page);
+    const auth = new AuthPage(page);
+
+    // importing data from test.json and sending to signUp method
+    await auth.signUp(
+      Data.happyPath.zip,
+      Data.happyPath.firstName,
+      Data.happyPath.lastName,
+      Data.happyPath.email,
+      Data.happyPath.password,
+    );
+
+    // Assertion to verify account creation
+    await auth.expectSignUpSuccess(Data.happyPath.successMessage);
+
+    await page.screenshot({ path: "./screenshots/test-1.png" });
+    await testInfo.attach("Account created successfully", {
+      path: "./screenshots/test-1.png",
+      contentType: "image/png",
     });
+  });
 
-    // Below test is for successful creation of account
-    test("Happy Path Account Creation @smoke @ui", async ({ page },testInfo) => {
+  test("Sad Path Account Creation @ui", async ({ page }, testInfo) => {
+    // const home = new HomePage(page);
+    const auth = new AuthPage(page);
 
-        // home and auth are objects created using the classes mentioned below
-        // const home = new HomePage(page);
-        const auth = new AuthPage(page);
+    // await home.open();
+    // await home.clickEnter();
+    await auth.signUp(
+      Data.sadPath.zip,
+      Data.sadPath.firstName,
+      Data.sadPath.lastName,
+      Data.sadPath.email,
+      Data.sadPath.password,
+    );
 
-        //opens home page of sharelane
-        // await home.open();
-        // await page.pause()
+    await auth.expectSignUpFailure(Data.sadPath.errorMessage);
 
-        // await home.clickEnter();
-
-        // importing data from test.json and sending to signUp method
-        await auth.signUp(
-            Data.happyPath.zip,
-            Data.happyPath.firstName,
-            Data.happyPath.lastName,
-            Data.happyPath.email,
-            Data.happyPath.password,
-        );
-
-        // Assertion to verify account creation
-        await auth.expectSignUpSuccess(Data.happyPath.successMessage);
-
-         await page.screenshot({ path: './screenshots/test-1.png' });
-         await testInfo.attach('Account created successfully', {
-            path: './screenshots/test-1.png',
-            contentType: 'image/png'
-        });
+    await page.screenshot({ path: "./screenshots/test-2.png" });
+    await testInfo.attach("Showing Error Message", {
+      path: "./screenshots/test-2.png",
+      contentType: "image/png",
     });
+  });
 
-    test("Sad Path Account Creation @ui", async ({ page },testInfo) => {
-        // const home = new HomePage(page);
-        const auth = new AuthPage(page);
+  test("@regression Full Bug Submission Flow @e2e", async ({
+    page,
+  }, testInfo) => {
+    // const home = new HomePage(page);
+    const portal = new PortalPage(page);
+    const auth = new AuthPage(page);
+    const bug = new BugPage(page);
 
-        // await home.open();
-        // await home.clickEnter();
-        await auth.signUp(
-            Data.sadPath.zip,
-            Data.sadPath.firstName,
-            Data.sadPath.lastName,
-            Data.sadPath.email,
-            Data.sadPath.password,
-        );
+    // await home.open();
+    await page.pause();
 
-        await auth.expectSignUpFailure(Data.sadPath.errorMessage);
-         
-         await page.screenshot({ path: './screenshots/test-2.png' });
-        await testInfo.attach('Showing Error Message', {
-            path: './screenshots/test-2.png',
-            contentType: 'image/png'
-        });
+    // await home.clickEnter();
+    await portal.openTestPortal();
+    await portal.openTrainingBTS();
+    await portal.openSubmitBug();
+    await auth.createAccountAutoLogin();
 
+    await portal.openTestPortal();
+    await portal.openTrainingBTS();
+
+    await portal.openSubmitBug();
+    await bug.populateResolutionOptions(); // Scan and populate dropdown values
+    await bug.submitBug(
+      "Login page not reloaded properly",
+      "When I enter the username and password the login page is not loading properly.",
+    );
+    // await bug.updateBug();
+    // await page.pause()
+
+    await page.screenshot({ path: "./screenshots/test-3.png" });
+    await testInfo.attach("Bug Submission", {
+      path: "./screenshots/test-3.png",
+      contentType: "image/png",
     });
+  });
 
-    // test('@smoke Account Setup Flow', async ({ page }) => {
-    //   const home = new HomePage(page);
-    //   const portal = new PortalPage(page);
-    //   const auth = new AuthPage(page);
+  test("Register and Order Flow @ui @e2e", async ({ page }, testInfo) => {
+    // const home = new HomePage(page);
+    await page.pause();
 
-    //   await home.open();
-    //   await home.clickEnter();
-    //   await portal.openTestPortal();
-    //   await portal.openTrainingBTS();
-    //   await portal.openSubmitBug();
-    //   await auth.createAccountAutoLogin();
-    //   // await page.pause()
-    // });
+    const auth = new AuthPage(page);
+    const portal = new PortalPage(page);
+    const order = new OrderPage(page);
+    const card = new CreditCardPage(page);
 
-    test("@regression Full Bug Submission Flow @e2e", async ({ page },testInfo) => {
-        // const home = new HomePage(page);
-        const portal = new PortalPage(page);
-        const auth = new AuthPage(page);
-        const bug = new BugPage(page);
+    // await home.open();
+    // await home.clickEnter();
+    await auth.signUp(
+      Data.happyPath.zip,
+      Data.happyPath.firstName,
+      Data.happyPath.lastName,
+      Data.happyPath.email,
+      Data.happyPath.password,
+    );
 
-        // await home.open();
-        await page.pause();
+    const creds = await auth.getCredentials();
+    await auth.login(creds.email, creds.password);
 
-        // await home.clickEnter();
-        await portal.openTestPortal();
-        await portal.openTrainingBTS();
-        await portal.openSubmitBug();
-        await auth.createAccountAutoLogin();
+    await order.addBookAndCheckout();
+    await portal.openTestPortal();
+    await portal.openCreditCardGenerator();
+    const cardNumber = await card.generateCard();
+    await card.pay(cardNumber);
 
-        await portal.openTestPortal();
-        await portal.openTrainingBTS();
+    await expect(page.getByText(Data.bookOrder.orderSuccess)).toBeVisible();
+    // await page.pause()
 
-        await portal.openSubmitBug();
-        await bug.populateResolutionOptions(); // Scan and populate dropdown values
-        await bug.submitBug(
-            "Login page not reloaded properly",
-            "When I enter the username and password the login page is not loading properly.",
-        );
-        // await bug.updateBug();
-        // await page.pause()
-        
-           await page.screenshot({ path: './screenshots/test-3.png' });
-           await testInfo.attach('Bug Submission', {
-            path: './screenshots/test-3.png',
-            contentType: 'image/png'
-        });
-
+    await page.screenshot({ path: "./screenshots/test-4.png" });
+    await testInfo.attach("Register and order", {
+      path: "./screenshots/test-4.png",
+      contentType: "image/png",
     });
-
-    test("Register and Order Flow @ui @e2e", async ({ page },testInfo) => {
-        // const home = new HomePage(page);
-        await page.pause()
-
-        const auth = new AuthPage(page);
-        const portal = new PortalPage(page);
-        const order = new OrderPage(page);
-        const card = new CreditCardPage(page);
-
-        // await home.open();
-        // await home.clickEnter();
-        await auth.signUp(
-            Data.happyPath.zip,
-            Data.happyPath.firstName,
-            Data.happyPath.lastName,
-            Data.happyPath.email,
-            Data.happyPath.password,
-        );
-
-        const creds = await auth.getCredentials();
-        await auth.login(creds.email, creds.password);
-
-        await order.addBookAndCheckout();
-        await portal.openTestPortal();
-        await portal.openCreditCardGenerator();
-        const cardNumber = await card.generateCard();
-        await card.pay(cardNumber);
-
-        await expect(page.getByText(Data.bookOrder.orderSuccess)).toBeVisible();
-        // await page.pause()
-
-         await page.screenshot({ path: './screenshots/test-4.png' });
-           await testInfo.attach('Register and order', {
-            path: './screenshots/test-4.png',
-            contentType: 'image/png'
-        });
-    });
+  });
 });
